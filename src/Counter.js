73cff018582deae6c9847191e414a2c9aa3f647e
Roni Raj Kamal Pradhan <ronirajkamalpradhan1112@gmail.com>
2022-03-07 21:13:16 +0530

    changes

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { decrement, increment, set } from './actions';
+import { SetCounter } from './SetCounter';
+import { useActions } from './use-actions';
+import { useCounter } from './use-counter';
 export const Counter = () => {
   const incident = 'Incident';
-  const count = 0;
+  const { count, increment, decrement, set } = useCounter();
 
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button>Increment</button>
-        <button>Reset</button>
-        <button>Decrement</button>
+        <button onClick={() => increment(1)}>Increment</button>
+        <button onClick={() => set(0)}>Reset</button>
+        <button onClick={() => decrement(1)}>Decrement</button>
       </section>
+      <SetCounter />
     </main>
   );
 };
